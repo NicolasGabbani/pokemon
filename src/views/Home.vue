c79a69de17b269fad76d2455708001843a66ed8c
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-04-21 16:05:45 +0200

    better render

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="site">
-    <template v-if="showPkm">
+    <div
+      class="modal-content animate__animated animate__faster"
+      :class="{ animate__fadeOut: !showPkm, animate__fadeIn: showPkm }"
+    >
       <router-view @closeModal="closeModal"></router-view>
-    </template>
-    
+    </div>
+
     <h1>Pokemon 1ère génération</h1>
     <Loading :loading="loading" />
     <template v-if="!loading">
       <div class="filters">
-        <input type="text" v-model="filter_search" placeholder="Recherchez un pokémon...">
+        <input
+          type="text"
+          v-model="filter_search"
+          placeholder="Recherchez un pokémon..."
+        />
         <select v-model="filter_type">
           <option value="">Filtrer par type...</option>
           <option value="steel">Acier</option>
@@ -31,21 +38,32 @@
         </select>
       </div>
       <div class="grid pokemons">
-        <div class="animate__animated animate__shakeX" v-if="noResult">pas de résultat</div>
+        <div class="animate__animated animate__shakeX" v-if="noResult">
+          pas de résultat
+        </div>
         <router-link
-          v-for="(pokemon, index) in (filteredPkms || orderedPkms)" 
-          :key="pokemon.id" 
+          v-for="(pokemon, index) in filteredPkms || orderedPkms"
+          :key="pokemon.id"
           :to="`/pokemon/${getID(pokemon)}`"
           class="animate__animated pokemons__pokemon"
-          :class="{'animate__fadeIn': !loading || !noResult}"
+          :class="{ animate__fadeIn: !loading || !noResult }"
           @mouseenter.native="hoverImg = index"
           @mouseleave.native="hoverImg = null"
         >
-          <div class="pokemon-id">#{{getID(pokemon)}}</div>
-          <div class="pokemon-name">{{getName(pokemon)}}</div>
-          <img :src="hoverImg == index ? getSpriteShiny(pokemon) : getSprite(pokemon)" :alt="getName(pokemon)" class="pokemon-img">
+          <div class="pokemon-id">#{{ getID(pokemon) }}</div>
+          <div class="pokemon-name">{{ getName(pokemon) }}</div>
+          <img
+            :src="hoverImg == index ? getSpriteShiny(pokemon) : getSprite(pokemon)"
+            :alt="getName(pokemon)"
+            class="pokemon-img"
+          />
           <div class="pokemon-types">
-            <span class="pokemon-type" :class="type" v-for="type in pokemon.types" :key="type"></span>
+            <span
+              class="pokemon-type"
+              :class="type"
+              v-for="type in pokemon.types"
+              :key="type"
+            ></span>
           </div>
         </router-link>
       </div>
@@ -54,12 +72,12 @@
 </template>
 
 <script>
-import axios from 'axios'
-import _ from 'lodash'
-import 'animate.css'
-import Loading from '@/components/Loading.vue'
+import axios from "axios";
+import _ from "lodash";
+import "animate.css";
+import Loading from "@/components/Loading.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Loading,
   },
@@ -70,97 +88,110 @@ export default {
       filteredPkms: null,
       loading: true,
       hoverImg: null,
-      filter_search: '',
-      filter_type: '',
+      filter_search: "",
+      filter_type: "",
       noResult: false,
-      showPkm: false
-    }
+      showPkm: false,
+    };
   },
   mounted() {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=151/`
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=151/`;
     axios
       .get(url)
-      .then(response => (this.pokemons = response.data.results))
+      .then((response) => (this.pokemons = response.data.results))
       .finally(() => {
-        for(let pokemon in this.pokemons){
-          this.getPokemon(this.pokemons[pokemon])
+        for (let pokemon in this.pokemons) {
+          this.getPokemon(this.pokemons[pokemon]);
         }
-        this.loading = false
-      })
+        this.loading = false;
+      });
   },
   methods: {
-    getPokemon(pkm){
-      let pokemon = null
-      let infoPkm = null
+    getPokemon(pkm) {
+      let pokemon = null;
+      let infoPkm = null;
       axios
         .get(pkm?.url)
-        .then(response => (pokemon = response.data))
+        .then((response) => (pokemon = response.data))
         .finally(() => {
           axios
             .get(pokemon?.species.url)
-            .then(response => infoPkm = response.data)
+            .then((response) => (infoPkm = response.data))
             .finally(() => {
-              this.arrayPokemons.push(infoPkm)
-              this.arrayPokemons.find(p => p.id == pokemon.id).sprite = pokemon?.sprites.front_default
-              this.arrayPokemons.find(p => p.id == pokemon.id).sprite_shiny = pokemon?.sprites.front_shiny
-              let typesArr = []
-              for(let index in pokemon?.types){
-                typesArr.push(pokemon?.types[index].type.name)
+              this.arrayPokemons.push(infoPkm);
+              this.arrayPokemons.find((p) => p.id == pokemon.id).sprite =
+                pokemon?.sprites.front_default;
+              this.arrayPokemons.find((p) => p.id == pokemon.id).sprite_shiny =
+                pokemon?.sprites.front_shiny;
+              let typesArr = [];
+              for (let index in pokemon?.types) {
+                typesArr.push(pokemon?.types[index].type.name);
               }
-              this.arrayPokemons.find(p => p.id == pokemon.id).types = typesArr
-            })
-        })
+              this.arrayPokemons.find((p) => p.id == pokemon.id).types = typesArr;
+            });
+        });
     },
-    getName(pkm){
-      return pkm?.names.find(name => name.language.name == 'fr').name
+    getName(pkm) {
+      return pkm?.names.find((name) => name.language.name == "fr").name;
     },
-    getSprite(pkm){
-      return pkm.sprite
+    getSprite(pkm) {
+      return pkm.sprite;
     },
-    getSpriteShiny(pkm){
-      return pkm.sprite_shiny
+    getSpriteShiny(pkm) {
+      return pkm.sprite_shiny;
     },
-    getID(pkm){
-      return pkm.id
+    getID(pkm) {
+      return pkm.id;
     },
-    closeModal(){
-      this.showPkm = false
-      this.$router.replace('/')
+    closeModal() {
+      this.showPkm = false;
+      setTimeout(() => {
+        this.$router.replace("/");
+      }, 500);
     },
-    filterName(pkm, val){
-      return val == '' ? true : this.getName(pkm).toLowerCase().includes(val.toLowerCase()) || pkm.id == val
+    filterName(pkm, val) {
+      return val == ""
+        ? true
+        : this.getName(pkm).toLowerCase().includes(val.toLowerCase()) || pkm.id == val;
     },
-    filterType(pkm, val){
-      return val == '' ? true : pkm.types.includes(val)
+    filterType(pkm, val) {
+      return val == "" ? true : pkm.types.includes(val);
     },
-    filter(){
-      if(this.$timer){
-        clearTimeout(this.$timer)
+    filter() {
+      if (this.$timer) {
+        clearTimeout(this.$timer);
       }
       this.$timer = setTimeout(() => {
-        this.filteredPkms = _.orderBy(this.arrayPokemons.filter(pkm => this.filterName(pkm, this.filter_search) && this.filterType(pkm, this.filter_type)), 'id')
-        this.noResult = !this.filteredPkms.length
-      }, 500)
-    }
+        this.filteredPkms = _.orderBy(
+          this.arrayPokemons.filter(
+            (pkm) =>
+              this.filterName(pkm, this.filter_search) &&
+              this.filterType(pkm, this.filter_type)
+          ),
+          "id"
+        );
+        this.noResult = !this.filteredPkms.length;
+      }, 500);
+    },
   },
   computed: {
     orderedPkms() {
-      return _.orderBy(this.arrayPokemons, 'id')
-    }
+      return _.orderBy(this.arrayPokemons, "id");
+    },
   },
   watch: {
-    filter_search: function(){
-      this.filter()
+    filter_search: function () {
+      this.filter();
     },
-    filter_type: function(){
-      this.filter()
+    filter_type: function () {
+      this.filter();
     },
     $route: {
       immediate: true,
-      handler: function(val) {
+      handler: function (val) {
         this.showPkm = val?.meta.showPkm;
-      }
+      },
     },
-  }
-}
+  },
+};
 </script>
--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="modal d-flex">
+  <div class="modal d-flex animate__animated">
     <Loading :loading="loading" />
-    <div v-if="!loading" class="this-pokemon">
+    <div
+      v-if="!loading"
+      class="this-pokemon animate__animated"
+      :class="{ animate__fadeIn: !loading }"
+    >
       <div class="col">
         <button class="back" @click.prevent="$emit('closeModal')">X</button>
-        <h1>{{namePkm}}</h1>
-        <p class="text-center">{{descPkm}}</p>
+        <h1>{{ namePkm }}</h1>
+        <p class="text-center">{{ descPkm }}</p>
         <p class="types text-center">
-          <span class="type" :class="type.en" v-for="type in typesPkm" :key="type.en">{{type.fr}}</span>
+          <span class="type" :class="type.en" v-for="type in typesPkm" :key="type.en">{{
+            type.fr
+          }}</span>
         </p>
       </div>
       <div class="col">
-        <img :src="this.getSprite()" :alt="namePkm">
+        <img :src="this.getSprite()" :alt="namePkm" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Loading from '@/components/Loading.vue'
+import axios from "axios";
+import Loading from "@/components/Loading.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Loading
+    Loading,
   },
   props: {
-    closeModal: Function
+    closeModal: Function,
   },
   data() {
     return {
@@ -37,109 +43,114 @@ export default {
       typesPkmTmp: [],
       typesPkm: [],
       spritePkm: null,
-      id: this.$route.params.id
-    }
+      id: this.$route.params.id,
+    };
   },
   mounted() {
-    this.loadPkm()
+    this.loadPkm();
   },
   methods: {
-    loadPkm(){
-      this.loading = true
-      const url = `https://pokeapi.co/api/v2/pokemon/${this.id}`
+    loadPkm() {
+      this.loading = true;
+      const url = `https://pokeapi.co/api/v2/pokemon/${this.id}`;
       axios
         .get(url)
-        .then(response => (this.pokemon = response.data))
+        .then((response) => (this.pokemon = response.data))
         .finally(() => {
-          for(let index in this.pokemon?.types){
-            this.typesPkmTmp.push(this.pokemon?.types[index].type)
+          for (let index in this.pokemon?.types) {
+            this.typesPkmTmp.push(this.pokemon?.types[index].type);
           }
-          this.getInfo()
-          this.loading = false
-        })
+          this.getInfo();
+          this.loading = false;
+        });
     },
-    async getInfo(){
-      let infoPkm = null
-      let tyPkm = null
-      for(let index in this.typesPkmTmp){
+    async getInfo() {
+      let infoPkm = null;
+      let tyPkm = null;
+      for (let index in this.typesPkmTmp) {
         await axios
           .get(this.typesPkmTmp[index]?.url)
-          .then(response => tyPkm = response.data)
+          .then((response) => (tyPkm = response.data))
           .finally(() => {
             this.typesPkm[index] = {
               en: this.typesPkmTmp[index].name,
-              fr: (tyPkm.names.find(t => t.language.name == 'fr').name)
-            }
-          })
+              fr: tyPkm.names.find((t) => t.language.name == "fr").name,
+            };
+          });
       }
       await axios
         .get(this.pokemon?.species.url)
-        .then(response => infoPkm = response.data)
+        .then((response) => (infoPkm = response.data))
         .finally(() => {
-          this.namePkm = infoPkm?.names.find(name => name.language.name == 'fr').name
-          this.descPkm = infoPkm?.flavor_text_entries.find(desc => desc.language.name == 'fr' && desc.version.name == 'lets-go-pikachu').flavor_text
-        })
+          this.namePkm = infoPkm?.names.find((name) => name.language.name == "fr").name;
+          this.descPkm = infoPkm?.flavor_text_entries.find(
+            (desc) => desc.language.name == "fr" && desc.version.name == "lets-go-pikachu"
+          ).flavor_text;
+        });
     },
-    getSprite(){
-      return this.pokemon?.sprites.other['official-artwork'].front_default
-    }
+    getSprite() {
+      return this.pokemon?.sprites.other["official-artwork"].front_default;
+    },
   },
   watch: {
     $route(to) {
-      this.id = to.params.id
-      this.loadPkm()
-    }
-  }
-}
+      this.id = to.params.id;
+      this.loadPkm();
+    },
+  },
+};
 </script>
 
 <style lang="sass">
-  @media screen and (max-width:375px)
-    .modal
-      height: 90vh !important
+@media screen and (max-width:375px)
   .modal
-    width: min(90vw, 900px)
-    height: 70vh
-    position: fixed
-    top: 50%
-    left: 50%
-    z-index: 99
-    transform: translate(-50%, -50%)
-    background: #F1F1F1
-    border: 1px solid #AAA
-    padding: 30px
+    height: 90vh !important
+.modal-content
+  position: relative
+  z-index: 99
+.modal
+  width: min(90vw, 900px)
+  height: 70vh
+  position: fixed
+  top: 50%
+  left: 50%
+  z-index: 99
+  transform: translate(-50%, -50%)
+  background: #F1F1F1
+  border: 1px solid #AAA
+  padding: 30px
+  border-radius: 10px
+.this-pokemon
+  display: grid
+  grid-template-columns: repeat(auto-fit, minmax(130px, 1fr))
+  gap: 50px
+.col
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-content: center
+.types
+  margin-top: 30px
+  display: flex
+  justify-content: center
+  align-content: center
+  gap: 15px
+  width: 100%
+  .type
+    padding: 6px 12px
+    color: white
     border-radius: 10px
-  .this-pokemon
-    display: grid
-    grid-template-columns: repeat(auto-fit, minmax(130px, 1fr))
-    gap: 50px
-  .col
-    display: flex
-    flex-direction: column
-    justify-content: center
-    align-content: center
-  .types
-    margin-top: 30px
-    display: flex
-    justify-content: center
-    align-content: center
-    gap: 15px
-    width: 100%
-    .type
-      padding: 6px 12px
-      color: white
-      border-radius: 10px
 
-  .back
-    position: absolute
-    top: 10px
-    right: 10px
-    text-decoration: none
-    text-transform: uppercase
-    color: black
-    font-weight: bold
-    border: none
-    outline: none
-    font-size: 2rem
-    cursor: pointer
+.back
+  position: absolute
+  top: 10px
+  right: 10px
+  text-decoration: none
+  text-transform: uppercase
+  color: black
+  font-weight: bold
+  border: none
+  outline: none
+  font-size: 2rem
+  cursor: pointer
 </style>
